ddc9c2c04703ce8ed63be5fea59e0d744d9c0ad2
zizi62 <43918051+zizi62@users.noreply.github.com>
2018-10-25 06:52:12 -0400

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,1 @@
+"use strict";document.querySelector(".slider__scroll").addEventListener("click",function(){var t=document.documentElement.clientHeight;!function e(){window.pageYOffset<t&&(window.scrollBy(0,10),setTimeout(e,0)),window.pageYOffset>t&&window.scrollTo(0,t)}()});
