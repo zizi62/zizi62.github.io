4c09fdaa0a15f952a90c10b99677842b7b8ed024
zizi62 <43918051+zizi62@users.noreply.github.com>
2018-11-18 17:53:24 -0500

    Delete scroll.js

--- js/scroll.js
+++
@@ -1,1 +0,0 @@
-"use strict";document.querySelector(".slider__scroll").addEventListener("click",function(){var t=document.documentElement.clientHeight;!function e(){window.pageYOffset<t&&(window.scrollBy(0,10),setTimeout(e,0)),window.pageYOffset>t&&window.scrollTo(0,t)}()});
