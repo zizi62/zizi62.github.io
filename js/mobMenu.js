e65cc1d25fbef01ff3a437d11ad77269fe4e34e2
zizi62 <43918051+zizi62@users.noreply.github.com>
2018-11-18 17:53:45 -0500

    Add files via upload

---
+++ js/mobMenu.js
@@ -0,0 +1,1 @@
+"use strict";!function(){var e=document.querySelector(".mob__menu_buttom"),t=document.querySelector(".menu");e.addEventListener("click",function(){t.classList.contains("menu__active")?t.classList.remove("menu__active"):t.classList.add("menu__active")})}();
